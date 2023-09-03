1f26d1b43197e6dabcf654d8d7b13317b4bdd578
Abir Hossen <99603168+AbirhossenCSE@users.noreply.github.com>
2023-09-03 16:31:40 +0600

    Add files via upload

---
+++ strfunction.php
@@ -0,0 +1,14 @@
+<?php
+// $x = "My name is khan";
+// $y = "Tomar";
+
+// echo strlen($x)."<br>";
+// echo str_word_count($x)."<br>";
+// echo strrev($x)."<br>";
+// echo str_replace("My","Amar",$x)."<br>";
+// echo str_replace('My', $y, $x);
+
+
+
+
+?>
---
+++ switchcase.php
@@ -0,0 +1,23 @@
+<?php
+
+// $color = 'blue';
+//     switch ($color) {
+//         case 'green':
+//             echo 'color is green';
+//             break;
+//          case 'red':
+//             echo 'color is red';
+//             break;
+//         case 'yellow':
+//             echo 'color is yellow';
+//             break;
+        
+//         default:
+//             echo 'No color found';
+//             break;
+//     }
+
+date_default_timezone_set('Asia/dhaka');
+$time = date('d/m/Y');
+echo $time;
+?>
