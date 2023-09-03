e10957cd736e640612b176bc17c259b81abfc36e
Abir Hossen <99603168+AbirhossenCSE@users.noreply.github.com>
2023-09-03 16:28:43 +0600

    Add files via upload

---
+++ condition.php
@@ -0,0 +1,39 @@
+<?php
+
+// $a = 10;
+// $b = 12;
+
+// if($a > $b){
+//     echo "A is Big";
+// }
+// else if($b > $a){
+//     echo "B is Big";
+// }
+// elseif($a == $b){
+//     echo "Equal";
+// }
+// else{
+//     echo "Nothing";
+// }
+
+// $age = 32;
+//     if($age >= 18){
+//         if($age <= 30){
+//             echo "Yes";
+//         }
+//         else{
+//             echo "Nikal";
+//         }
+//     }
+//     else{
+//         echo "Cannot Acces";
+//     }
+
+
+// $age = 12;
+// echo($age >=18?"You can play":"No you can't");
+
+
+
+
+?>
