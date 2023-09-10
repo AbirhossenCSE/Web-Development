3c2f178630eb49ee3dda3fdd3671efc479484de7
Abir Hossen <99603168+AbirhossenCSE@users.noreply.github.com>
2023-09-10 21:08:15 +0600

    Add files via upload

---
+++ strf.php
@@ -0,0 +1,10 @@
+<?php
+$a = "My home town is Kushtia";
+$b = "Kushtia is under Khulna division";
+
+echo strlen($a)."<br>";
+echo str_word_count($a)."<br>";
+echo strrev($b).'<br>';
+echo str_replace("My","Amr",$a)."<be";
+
+?>
