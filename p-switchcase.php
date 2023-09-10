9fd477ca915b76465d46b21d3141c56d913d7de9
Abir Hossen <99603168+AbirhossenCSE@users.noreply.github.com>
2023-09-10 21:09:29 +0600

    switchcase

---
+++ p-switchcase.php
@@ -0,0 +1,16 @@
+<?php
+$character = "nosto";
+switch ($character) {
+    case 'good':
+        echo "Abir is a good boy";
+        break;
+    case 'Playboy':
+        echo "Raisul is a playboy";
+    case 'nosto':
+        echo "Sajib nosto pula";
+        break;
+    default:
+        echo "kicuna";
+        break;
+}
+?>
